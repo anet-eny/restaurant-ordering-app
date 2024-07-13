363ba633aaa82662b3c7443575dd67e47767fcbc
anet-eny <anet.eny@gmail.com>
2024-07-13 20:58:21 +0200

    Create getMenuHtml() and dynamically render menu html

---
+++ script.js
@@ -0,0 +1,28 @@
+import { menuArray } from "./data.js";
+
+function getMenuHtml (menuArray) {
+    return menuArray.map(item => {
+        const {
+            name,
+            ingredients,
+            id,
+            price,
+            emoji
+        } = item
+        return `
+        <div class="item">
+                <div class="left-content">
+                    <span class="emoji">${emoji}</span>
+                    <div class="item-inner">
+                        <h2>${name}</h2>
+                        <p class="item-desc">${ingredients}</p>
+                        <p class="${price}">$14</p> 
+                    </div>
+                </div>
+                <button class="add-btn">+</button> 
+        </div>
+        `
+    }).join("")
+}
+
+document.getElementById('menu').innerHTML = getMenuHtml(menuArray)
