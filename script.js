2664ae52254a583e08a44b66ce4ee6a86198f464
anet-eny <anet.eny@gmail.com>
2024-07-14 17:00:37 +0200

    Create getCartHtml() and renderCart() to render cart with 1 item

--- script.js
+++ script.js
@@ -12,9 +12,53 @@ function addToCart(itemId) {
     const targetItemObj = menuArray.filter(function(item){
         return itemId === item.id.toString()
     })[0]
-    cartArr.push(targetItemObj)
-    console.log(cartArr)
+    if (targetItemObj){
+        cartArr.push(targetItemObj)
+    }
+    if (cartArr.length === 1) {
+        getCartHtml()
+    } else if (cartArr.length > 1) {
+        addCartHtml()
+    }
 
+}
+
+function getCartHtml() {
+    let cartHtml = ''
+    cartArr.map(item => {
+        const {
+            name,
+            price
+        } = item
+        
+        cartHtml += `
+        <h2 class="order-h2">Your order</h2>
+                <div class="order-list">
+                    <div class="left-content">
+                        <h2>${name}</h2>
+                        <button class="remove-btn">remove</button>
+                    </div>
+                    <p class="item-price">$${price}</p> 
+                </div>
+               
+                <div class="total">
+                    <h2>Total price:</h2>
+                    <p class="item-price">$14</p> 
+                </div>
+                <button class="purchase-btn">Complete order</button>
+        `
+        
+    })
+    return renderCart(cartHtml)
+}
+
+function addCartHtml() {
+
+}
+
+
+function renderCart(renderItem) {
+    document.getElementById('order-el').innerHTML = renderItem
 }
 
 
